fc8339c43c98f9252528a10dea52ff84d2fd51d1
Kathleen <kathleen60603@gmail.com>
2023-08-25 11:23:47 +0800

    first

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print("Hello Python!")
+import matplotlib.pyplot as plt
+import random
+
+x = []
+y = []
+c = []
+for i in range(10):
+    x.append(random.randint(1,20))
+    y.append(random.randint(1,20))
+    c.append(random.randint(5, 200))
+
+plt.scatter(x, y, color="red")
+plt.savefig('scatter.jpg')
+plt.show()
